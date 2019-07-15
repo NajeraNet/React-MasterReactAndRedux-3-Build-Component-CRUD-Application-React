7e97d0cba1c366c470fca7e79af40637ca219789
NajeraNet <najeranet.com@gmail.com>
2019-07-15 17:30:10 -0500

    no there to mach changes

--- src/App.js
+++ src/App.js
@@ -10,6 +10,11 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          <h3>And add new attributes to our app.js file</h3>
+          <br />          
+        </p>
+        <img id="Image-in-the-index" src="https://reactjsexample.com/content/images/2019/07/watermark-enhancer.jpg" alt="Creating code" className="App-header"></img>
         <a
           className="App-link"
           href="https://reactjs.org"
